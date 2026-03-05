ea77aa39bbbb7709b57d0da49c3148f757ceab3d
Lance Ivan Listana <lanceivanlistana15@gmail.com>
2026-03-05 08:42:12 +0800

    module update

--- backend/src/modules/admin-modules/ticket-status/ticket-status.module.ts
+++ backend/src/modules/admin-modules/ticket-status/ticket-status.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketStatusResolver } from './ticket-status.resolver';
 import { TicketStatusService } from './ticket-status.service';
+import { TicketsTbl } from 'src/modules/client-modules/tickets/entity/tickets.tbl';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TicketsTbl])],
   providers: [TicketStatusResolver, TicketStatusService]
 })
 export class TicketStatusModule {}
