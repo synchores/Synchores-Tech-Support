b683f315356b0493137bf49d50a982d0117e9d9e
Dhaniel Lofamia <dhanieeel0907@gmail.com>
2026-03-05 09:07:28 +0800

    ticket module ni lance

--- backend/src/modules/admin-modules/ticket-status/ticket-status.module.ts
+++ backend/src/modules/admin-modules/ticket-status/ticket-status.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketStatusResolver } from './ticket-status.resolver';
 import { TicketStatusService } from './ticket-status.service';
 import { TicketsTbl } from 'src/modules/client-modules/tickets/entity/tickets.tbl';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TicketsTbl])],
+  imports: [
+    TypeOrmModule.forFeature([TicketsTbl])
+  ],
   providers: [TicketStatusResolver, TicketStatusService]
 })
 export class TicketStatusModule {}
